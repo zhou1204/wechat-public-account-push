df4808547ace4bd1c05a9b5b5b7ab911f201acfd
zhou1204 <112380189+zhou1204@users.noreply.github.com>
2022-08-29 16:11:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,8 +6,7 @@ export const config = {
     // 公众号APP_ID
     APP_ID: "wx25d39603794ddf9a",
     // 公众号APP_SECRET
-    APP_SECRET: "0b947cefb6b0fc65056264fe8c07568b
-",
+    APP_SECRET: "0b947cefb6b0fc65056264fe8c07568b",
     // 模板消息id
     TEMPLATE_ID: "perDQ_i0FU_lf_qMLdbtwgXevhSNUbsNDPDuiSwJXhg",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
@@ -28,14 +27,14 @@ export const config = {
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "老婆", "year": "1993", "date": "12-27","type":'new'},
+      {"name": "家公", "year": "1993", "date": "08-09","type":'new'},
     ],
 
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+    LOVE_DATE: "2022-08-29",
     // 结婚纪念日
     MARRY_DATE: "2020-01-04",
 
@@ -44,7 +43,7 @@ export const config = {
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
+    LITERARY_PREFERENCE: "生日快乐！"
 
 
     }
